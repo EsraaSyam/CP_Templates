eb44f0a578586816119624b9c0ac6543d926d998
Esraa Syam <112758974+EsraaSyam@users.noreply.github.com>
2022-09-03 15:33:40 +0200

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <limits.h>
+#include <algorithm>
+#include <math.h>
+#include <cctype>
+#include <cmath>
+#include <bits/stdc++.h>
+#include <string>
+#include <utility>
+#include<vector>
+#define ll long long
+#define ull unsigned long long
+#define fixed(n) fixed << setprecision(n)
+//#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define sz(x) int(x.size())
+#define charToInt(s) (s - '0')
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define fi first
+#define se second
+#define number_line iota(all(vec) , 1)
+#define nl "\n"
+#define pi 3.14159
+#define all(s) s.begin(), s.end()
+#define rall(v) v.rbegin() , v.rend()
+#define Mod 1'000'000'007
+#define INF 2'000'000'000
+#define Upper(s) transform(all(s), s.begin(), ::toupper);
+#define Lower(s) transform(all(s), s.begin(), ::tolower);
+using namespace std;
+void esraa()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    // freopen("func.in" , "r" , stdin);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+}
+void solve(){
+    
+}
+int main()
+{
+    esraa();
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
