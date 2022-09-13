f9cd6e282ce754ae639c355d54d7e0846c4022ec
Esraa Syam <112758974+EsraaSyam@users.noreply.github.com>
2022-09-13 16:21:00 +0200

    Create new_template.cpp

---
+++ new_template.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define nl "\n"
+#define fi first
+#define se second
+#define pi 3.14159
+#define ll long long
+#define odd(a)  (a&1)
+#define even(a)  !(a&1)
+#define Mod 1'000'000'007
+#define INF 2'000'000'000
+#define sz(x) int(x.size())
+#define charToInt(s) (s - '0')
+#define ull unsigned long long
+#define all(s) s.begin(), s.end()
+#define rall(v) v.rbegin() , v.rend()
+#define fixed(n) fixed << setprecision(n)
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define cin(vec) for(auto& i : vec) cin >> i
+#define to_decimal(bin) stoll(bin, nullptr, 2)
+#define Ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define Floor(n, m) (((n) / (m)) - ((n) % (m) ? 0 : 1))
+#define Upper(s) transform(all(s), s.begin(), ::toupper);
+#define Lower(s) transform(all(s), s.begin(), ::tolower);
+#define cout(vec) for(auto& i : vec) cout << i << " "; cout << "\n";
+#define cout_map(mp) for(auto& [f, s] : mp) cout << f << "  " << s << "\n";
+using namespace std;
+void esraa()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+#endif
+}
+void solve(){
+  
+} 
+int main()
+{
+    esraa();
+    int t = 1;
+   // cin >> t;
+    while(t--)
+      solve();
+    return 0;
+}
